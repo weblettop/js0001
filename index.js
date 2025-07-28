6151fdc647610b3eb8339c21b996630dfd81114e
weblettop <weblettop.gmail.com>
2025-07-28 18:28:08 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const daysOfWeek = [
+  "Понеділок",
+  "Вівторок",
+  "Середа",
+  "Четвер",
+  "П’ятниця",
+  "Субота",
+  "Неділя"
+];
+
+console.log("Дні тижня:");
+for (let i = 0; i < daysOfWeek.length; i++) {
+  console.log(`${i + 1}: ${daysOfWeek[i]}`);
+}
+
+///
+console.log("Парні дні тижня:");
+
+daysOfWeek.forEach((day, index) => {
+  if (index % 2 === 0) {
+    console.log(`${index + 1}: ${day}`);
+  }
+});
